345047034502497506820720bb61c29f02853ed7
Aman <kapuraman@gmail.com>
2019-11-19 11:15:45 +0800

    adding comment to BB application

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -1,8 +1,9 @@
 package com.stacksimplify.restservices;
+// this is just for testing commit functionality
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
